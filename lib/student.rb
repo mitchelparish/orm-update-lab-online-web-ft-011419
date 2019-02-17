6092f8198934c12cb81ee8e5911b1d92f3c3cd20
Mitchel Parish <mitchelparish@gmail.com>
2019-02-17 21:07:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,12 +52,12 @@ attr_reader :id
   end
 
   def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    new_student = self.new(row[1], row[2], row[3])
   end
+
+
+
+
 
   def self.find_by_name
     sql = <<-SQL
