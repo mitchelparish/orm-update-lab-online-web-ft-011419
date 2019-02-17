ce9cfb4829f2c54b416478b152a49d56f12325fa
Mitchel Parish <mitchelparish@gmail.com>
2019-02-17 20:57:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ attr_reader :id
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end 
+  end
 
   def self.create(name:, grade:)
     student = Student.new(name, grade)
