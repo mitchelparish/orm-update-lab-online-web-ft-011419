658e57c5cc1d3cda0fafdaca3546f4c759b514c3
Mitchel Parish <mitchelparish@gmail.com>
2019-02-17 21:13:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ attr_reader :id
      new_student = self.new(row[1], row[2], row[0])
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[1], result[2], result[0])
