de8ca74a105552b363ba484e94cae71fe3566c74
Mitchel Parish <mitchelparish@gmail.com>
2019-02-17 21:14:05 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
-  def initialize(id = nil, name, grade)
+  def initialize(name, grade, id = nil)
     @id = id
     @name = name
     @grade = grade
@@ -40,8 +40,24 @@ attr_reader :id
     end
   end
 
-  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 
+  def self.new_from_db(row)
+     new_student = self.new(row[1], row[2], row[0])
+  end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[1], result[2], result[0])
+  end
 end
