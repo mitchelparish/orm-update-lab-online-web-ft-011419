45a60774d3f1492172b1e2a6a2e63b70d18219c6
Mitchel Parish <mitchelparish@gmail.com>
2019-02-17 20:52:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,8 +44,15 @@ attr_reader :id
     student = Student.new(name, grade)
     student.save
     student
-  end 
+  end
 
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end 
 
 
 
