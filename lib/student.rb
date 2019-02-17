e5c9383c49f9ca5a5ce545a9228ccdc37ff520d1
Mitchel Parish <mitchelparish@gmail.com>
2019-02-17 21:00:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,6 +51,13 @@ attr_reader :id
     student
   end
 
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+   new_student.name = row[1]
+   new_student.grade = row[2]
+   new_student
+ end   
 
 
 
